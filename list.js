33128120d388772b61eb75725e93d74f680e037b
Ksawix0 <wierzchowskiksawery@gmail.com>
2025-08-04 15:08:08 +0200

    feat: added cached list/faster loading

--- list.js
+++ list.js
@@ -1,3 +1,10 @@
+let cookies={}
+if (document.cookie.length > 0){
+    document.cookie.split('; ').forEach( cookie  => {
+        let values = cookie.split('=')
+        cookies[values[0]] = values.slice(1).join('')
+    })
+}
 
 let site_name
 url = location.href.split('?')[0].split('/')
@@ -7,17 +14,32 @@ if (url[url.length - 1].endsWith('.html')) {
 else{
     site_name = url[url.length - 1]
 }
+document.addEventListener('DOMContentLoaded', () => {
+    let list = document.getElementById('list')
+    listing(cookies['old_list'].split(','), list)
+})
 
 fetch("../scripts/list.json")
     .then(response => response.json())
     .then(json => {
-
-
         let list = document.getElementById('list')
-        list.innerHTML = ""
-        console.log(json)
-        for (let i=0 ;i < json["list"][site_name].length ;i++){
-            list.innerHTML += "<li style=\"text-align: left; padding-left: 5%;\"><a href=\"" + json["list"][site_name][i] + "\" style=\"font-size: larger; color: white\">" + json["list"][site_name][i] + "</a></li>"
-        }
-
+        console.log(json, list)
+        listing(json['list'][site_name],list)
+        cookies['old_list'] = json["list"][site_name].join(",")
+        save_cookies()
     })
+
+function save_cookies() {
+    let cookiess = []
+    Object.keys(cookies).forEach(key => {
+        cookiess.push(key + "=" + cookies[key])
+        document.cookie = cookiess.join("; ")
+    })
+}
+
+function listing(list_f, list){
+    list.innerHTML = ''
+    for (let i=0 ;i < list_f.length ;i++){
+        list.innerHTML += "<li style=\"text-align: left; padding-left: 5%;\"><a href=\"" + list_f[i] + "\" style=\"font-size: larger; color: white\">" + list_f[i] + "</a></li>"
+    }
+}
